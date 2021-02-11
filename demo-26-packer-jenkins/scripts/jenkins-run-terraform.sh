54b9584a4e49d38b5858375d70201a50c40a9df8
Oleksandr P <alexander.persh@gmail.com>
2021-02-11 04:26:34 +0200

    Modify scripts/jenkins-run-terraform.sh

--- demo-26-packer-jenkins/scripts/jenkins-run-terraform.sh
+++ demo-26-packer-jenkins/scripts/jenkins-run-terraform.sh
@@ -6,5 +6,5 @@ S3_BUCKET=$(aws s3 ls --region $AWS_REGION|grep terraform-state|tail -n1|cut -d 
 sed -i "s/terraform-state-bb3cu2gr/${S3_BUCKET}/" backend.tf
 sed -i "s/#//g" backend.tf
 aws s3 cp s3://"${S3_BUCKET}"/amivar.tf amivar.tf --region "$AWS_REGION"
-/usr/bin/terraform init
+/usr/bin/terraform init -reconfigure
 /usr/bin/terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
