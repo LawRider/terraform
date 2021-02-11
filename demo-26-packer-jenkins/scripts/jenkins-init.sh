8ccb6ab0afb7c75f82d49159cf252f2565d6041d
Oleksandr P <alexander.persh@gmail.com>
2021-02-11 04:49:01 +0200

    Modify scripts/jenkins-init.sh

--- demo-26-packer-jenkins/scripts/jenkins-init.sh
+++ demo-26-packer-jenkins/scripts/jenkins-init.sh
@@ -3,20 +3,20 @@
 # volume setup
 vgchange -ay
 
-DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
-if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
+DEVICE_FS=$(blkid -o value -s TYPE "${DEVICE}")
+if [ "$(echo -n "$DEVICE_FS")" == "" ] ; then 
   # wait for the device to be attached
-  DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
+  DEVICENAME=$(echo "${DEVICE}" | awk -F '/' '{print $3}')
   DEVICEEXISTS=''
   while [[ -z $DEVICEEXISTS ]]; do
     echo "checking $DEVICENAME"
-    DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
+    DEVICEEXISTS=$(lsblk |grep -c "$DEVICENAME")
     if [[ $DEVICEEXISTS != "1" ]]; then
       sleep 15
     fi
   done
-  pvcreate ${DEVICE}
-  vgcreate data ${DEVICE}
+  pvcreate "${DEVICE}"
+  vgcreate data "${DEVICE}"
   lvcreate --name volume1 -l 100%FREE data
   mkfs.ext4 /dev/data/volume1
 fi
@@ -32,18 +32,18 @@ apt-get update
 # install dependencies
 apt-get install -y python3 openjdk-11-jdk awscli
 # install jenkins
-apt-get install -y jenkins=${JENKINS_VERSION} unzip
+apt-get install -y "jenkins=${JENKINS_VERSION}" unzip
 
 # install terraform
-wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
-&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+wget -q "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" \
+&& unzip -o "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -d /usr/local/bin \
+&& rm "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 
 # install packer
-cd /usr/local/bin
-wget -q https://releases.hashicorp.com/packer/0.10.2/packer_0.10.2_linux_amd64.zip
-unzip packer_0.10.2_linux_amd64.zip
+cd /usr/local/bin || exit
+wget -q https://releases.hashicorp.com/packer/1.6.6/packer_1.6.6_linux_amd64.zip
+unzip packer_1.6.6_linux_amd64.zip
 # clean up
 apt-get clean
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-rm packer_0.10.2_linux_amd64.zip
+rm "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+rm packer_1.6.6_linux_amd64.zip
