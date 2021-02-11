1d075e6bb087b46b333c28c3db8fb02f01d8f7f6
Oleksandr P <alexander.persh@gmail.com>
2021-02-11 03:44:23 +0200

    Modify demo-26-packer-jenkins

--- demo-26-packer-jenkins/backend.tf
+++ demo-26-packer-jenkins/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bb3cu2gr"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-bb3cu2gr"
+#    key    = "terraform.tfstate"
+#    region = "eu-central-1"
+#  }
+#}
--- demo-26-packer-jenkins/scripts/jenkins-run-terraform.sh
+++ demo-26-packer-jenkins/scripts/jenkins-run-terraform.sh
@@ -2,9 +2,9 @@
 set -ex
 AWS_REGION="eu-central-1"
 cd jenkins-packer-demo
-S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
-sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
-sed -i 's/#//g' backend.tf
-aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
+S3_BUCKET=$(aws s3 ls --region $AWS_REGION|grep terraform-state|tail -n1|cut -d ' ' -f3)
+sed -i "s/terraform-state-bb3cu2gr/${S3_BUCKET}/" backend.tf
+sed -i "s/#//g" backend.tf
+aws s3 cp s3://"${S3_BUCKET}"/amivar.tf amivar.tf --region "$AWS_REGION"
 terraform init
 terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
