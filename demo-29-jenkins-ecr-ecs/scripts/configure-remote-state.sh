52ef2c8dcc3877eecfae90f41c82ce78e61639fa
Oleksandr P <alexander.persh@gmail.com>
2021-02-12 04:15:35 +0200

    Modify demo-29-jenkins-ecr-ecs/README and demo-29-jenkins-ecr-ecs/scripts/configure-remote-state.sh

--- demo-29-jenkins-ecr-ecs/scripts/configure-remote-state.sh
+++ demo-29-jenkins-ecr-ecs/scripts/configure-remote-state.sh
@@ -6,7 +6,7 @@ echo 'backend.tf
 ==========
 terraform {
   backend "s3" {
-    bucket = "terraform-state-a3c731f"
+    bucket = "terraform-state-19a2b621988"
     key    = "terraform.tfstate"
     region = "eu-central-1"
   }
